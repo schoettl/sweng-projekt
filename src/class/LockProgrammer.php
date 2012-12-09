21c7ff0c66644fb676f8e362c3751641003afa7b
Jakob Schöttl <jschoett@gmail.com>
2012-12-10 00:25:49 +0100

    LockProgrammer Impl added

--- src/class/LockProgrammer.php
+++ src/class/LockProgrammer.php
@@ -2,25 +2,57 @@
 class LockProgrammer
 {
     private $list; // Liste mit LockProgrammerConfig Objekten
+    private $it;
+    
+    function __construct()
+    {
+        $list = array();
+        $it = $list->getIterator();
+    }
+    
     function program($lock)
     {
-        foreach ($list as $cfg)
+        $i = $list->getIterator();
+        while ($i->valid()) {
+            $cfg = $i->current();
             if ($cfg->lockId == $lock->getLockId()) {                
-                $lock->setConfig($cfg->getLockConfig);
+                $lock->setConfig($cfg->lockConfig);
                 $cfg->inSync = true;
+                $it = $i; // iterator fuer location neu setzen
                 break;
             }
+            $i->next();
+        }
+        // Wenn keine updates fuer $lock da:
+        // auch kein problem!
     }
+    
     function nextLocation()
     {
         // weiter in liste, alle mit inSync == true ueberspringen
+        while ($it->valid() && $it->current()->inSync) {
+            $it->next();
+        }
+        // location zurueckgeben
+        if ($it->valid()) {
+            return $it->current()->location();
+        }
+        return false;
     }
-    function getLockProgrammerConfigs()
+    
+    function rewindLocation()
+    {
+        $it->rewind();
+    }
+    
+    function getConfigList()
     {
         return $this->list;
     }
-    function setLockProgrammerConfigs($list)
+    
+    function setConfigList($list)
     {
         $this->list = $list;
+        $it = $list->getIterator();
     }
 }
