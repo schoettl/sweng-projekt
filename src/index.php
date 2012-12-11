3fcf93b97411eaa98c9e8170a48eb082fc21030b
Jakob Schöttl <jschoett@gmail.com>
2012-12-11 19:09:06 +0100

    PHP-Dateien (Startseite)

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>sweng-projekt: Startseite</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <h1>sweng-projekt: Startseite</h1>
+        Folgende Seiten werden automatisch in einem neuen Tab geöffnet.
+        <ul>
+            <li><a href="init.php">System initialisieren: Das ganze System auf den Anfangszustand zurücksetzen und initialisieren</a></li>
+            <li><a href="ui/" target="_blank">Zur Benutzeroberfläche: Die GUI, so wie sie den Anwendern in Hotel oder Firma zur Verfügung steht</a></li>
+            <li><a href="sim/" target="_blank">Zur Simulation: Seiten zur Simulation von Zugangs-/Buchungssystem, Schlüssel, Schloss und Programmiervorgängen</a></li>
+        </ul>
+    </body>
+</html>
---
+++ src/init.php
@@ -0,0 +1,45 @@
+<?php
+require_once 'class/DBAccess.php';
+
+start_session();
+
+try {
+
+    // In DB: Alle Zugangseinträge löschen
+    $dbh = new DBAccess();
+    $dbh->exec("DELETE FROM whitelist WHERE LockId IS NOT NULL");
+    $dbh->exec("DELETE FROM blacklist WHERE LockId IS NOT NULL");
+    $dbh->exec("DELETE access WHERE AccessId IS NOT NULL");
+    $dbh->exec("UPDATE `lock` SET last_change = NOW(), last_sync = NULL 
+        WHERE LockId IS NOT NULL"); // last_change wird bei Aenderung zwar autom.
+        //aktualisiert, aber eben nur bei AENDERUNG. Deswegen explizit.
+
+    // TODO alle erforderlichen Objekte anlegen in Session.
+
+    
+    $success = true;
+} catch (Exception $e) {
+    $success = false;
+    $err = $e->getMessage();
+}
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>sweng-projekt: Initialisierung</title>
+    </head>
+    <body>
+        <h1>sweng-projekt: Initialisierung</h1>
+        <?php
+        if ($success) {
+            echo 'Initialisierung erfolgreich!';
+        } else {
+            echo 'Fehler: ' . htmlentities($err);
+            echo '<p><a href="init.php">Nochmal versuchen ...</a></p>';
+        }
+        ?>
+        <p><a href="./">Zurück zur Startseite</a></p>
+    </body>
+</html>
