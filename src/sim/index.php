4540a88c59f2faafa7eeb912941526cc9feea09c
Jakob Schöttl <jschoett@gmail.com>
2012-12-18 18:16:50 +0100

    Index files (sim/ui) and empty ban.php added

---
+++ src/sim/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>sweng-projekt: Simulation</title>
+    </head>
+    <body>
+        <h1>Simulation :: Startseite</h1>
+        <ul>
+            <li><a href="lock.php" >Türen/Schlösser ansehen, aufsperren, zumachen</a></li>
+            <li><a href="lockprogr.php" >Schlösser programmieren (mit Schloss-Programmiergerät)</a></li>
+        </ul>
+    </body>
+</html>
---
+++ src/ui/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>sweng-projekt: User interface</title>
+    </head>
+    <body>
+        <h1>User interface :: Startseite</h1>
+        <ul>
+            <li><a href="setaccess.php" >Zugang erstellen bzw. ändern</a></li>
+            <li><a href="sync.php" >Schloss-Programmiergerät synchronisieren</a></li>
+            <li><a href="ban.php" >Schlüssel sperren</a></li>
+        </ul>
+    </body>
+</html>
