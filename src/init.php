53ff2fd06663ec42f31253c3bc6bc6bbd22ed19b
Jakob Schöttl <jschoett@gmail.com>
2012-12-23 02:18:14 +0100

    HTML5 tweak

--- src/init.php
+++ src/init.php
@@ -29,7 +29,7 @@ try {
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta charset="UTF-8">
         <link rel="stylesheet" tyep="text/css" href="web.css" />
         <title>sweng-projekt: Initialisierung</title>
     </head>
