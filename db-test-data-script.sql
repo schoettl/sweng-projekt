42c0f7071f37939c63acbecd03a378171c15669f
Jakob Schöttl <jschoett@gmail.com>
2012-12-27 13:34:34 +0100

    Fixed wrong primary key ids

--- db-test-data-script.sql
+++ db-test-data-script.sql
@@ -11,9 +11,9 @@ insert into blacklist values (2,2);
 -- key 1: access fuer lock 2
 insert access values (1, 2, 1, NOW(), null);
 -- key 3: kein access fuer lock 2 mehr
-insert access values (1, 2, 3, NOW(), '2012-12-23 00:37:06');
+insert access values (2, 2, 3, NOW(), '2012-12-23 00:37:06');
 
 -- active key 25: access fuer lock 2
-insert access values (2, 2, 25, NOW(), null);
+insert access values (3, 2, 25, NOW(), null);
 
 select * from access;
