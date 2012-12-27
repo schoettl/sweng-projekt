573c95f853a82a4c69d4107c58605a89922180f6
Jakob Schöttl <jschoett@gmail.com>
2012-12-27 13:36:20 +0100

    Sim: Lock programmer finished

--- src/sim/lockprogr.php
+++ src/sim/lockprogr.php
@@ -15,16 +15,17 @@ $system = System::getInstance();
 $lp = $system->getLockProgrammer1();
 
 $lockid = getVarFromPostOrGet('lockid');
-
 $lock = $system->getLock($lockid);
-if (!$lock) {
-    $err[] = 'Ungültige LockId.';
-} else if (getVarFromPost('program')) {
-    $lp->program($lock);
-    $lp->nextLocation();
-}
 
-if (getVarFromPost('next')) {
+if (getVarFromPost('program')) {
+    if ($lock) {
+        $lp->program($lock);
+        $lp->nextLocation();
+        $success = true; // programmieren erfolgreich
+    } else {
+        $err[] = 'Ungültige LockId.';
+    }    
+} else if (getVarFromPost('next')) {
     $lp->nextLocation();
 } else if (getVarFromPost('rewind')) {
     $lp->rewindLocation();
@@ -32,51 +33,74 @@ if (getVarFromPost('next')) {
 
 $location = $lp->currentLocation();
 
-if ($location === false) $err[] = 'Keine weiteren Schlösser zu synchronisieren. Probieren Sie "Von vorne", falls Sie Schlösser übersprungen haben.';
+if ((getVarFromPost('program') || getVarFromPost('next') || getVarFromPost('rewind')) && $location) {
+    // Es wurde am Handheld was gedrückt (nicht rechts) -> Wähle autom. 'next location' als das Schloss, vor dem wir gerade stehen.
+    $lockid = $dbh->pquery("SELECT LockId FROM `lock` WHERE Location = ?", $location)->fetchColumn();
+}
 
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta charset="UTF-8">
         <link rel="stylesheet" tyep="text/css" href="../web.css" />
         <title>Lock Programmer</title>
     </head>
     <body>
         <h1>Schloss-Programmiergerät</h1>
-        <?php
-        foreach ($err as $e)
-            echo '<div class="err" >' . xsafe($e) . '</div>'; // div id und css fuer error messages
-        ?>
+        Nicht vergessen, das Schloss-Programmiergerät vorher <a href="../ui/sync.php">mit dem System zu synchronisieren</a>.
         <form method="POST" >
-            <input type="hidden" name="lockid" value="<?php xecho($lockid); /* damit die lock id nicht verloren geht */ ?>" />
+            <?php
+            if (isset($success))
+                echo '<div class="succ">Programmierung erfolgreich!</div>';
+            foreach ($err as $e)
+                echo '<div class="err" >' . xsafe($e) . '</div>'; // div id und css fuer error messages            
+            ?>
+            
             <table>
-                <tr>
-                    <td><input type="submit" name="next" value="Nächstes" /></td>                    
-                    <td><?php echo '<a href="lock.php?lockid=' . $lockid . '" >' . xsafe($location) . '</a>'; ?></td>
-                </tr>
-                <tr>
-                    <td><input type="submit" name="program" value="Programmieren" /></td>                    
-                </tr>
-                <tr>
-                    <td><input type="submit" name="rewind" value="Von vorne" /> (nur nicht-synchronisierte)</td>
-                </tr>
-            </table>
-            <table>
-                <tr>            
-                    <td>LockId:</td>
-                    <td><input type="text" name="lockid" value="<?php xecho($lockid); ?>"/></td>
-                    <td><input type="submit" name="apply" value="Schloss auswählen" /></td>
-                </tr>
                 <tr>
                     <td>
-                        <?php xecho($location); echo '<a href="lock.php?lockid=' . $lockid . '" >
-                            <img src="../img/lock' . ($lockid % 5) .
-                            'alt="Denk\' dir schönes Bild einer Türe mit einem Schloss." /></a>';
-                        ?>
+                        <div style="position: relative">            
+                            <div style="position: absolute; top: 250px; left: 300px">
+                                <p>
+                                    <input type="submit" name="next" value="Nächstes" />
+                                    <?php                                    
+                                    if ($location !== false) {
+                                        xecho($location . ' (nur ein Vorschlag)');
+                                    }
+                                    ?>
+                                </p>
+                                <p><input type="submit" name="program" value="Programmieren" /></p>
+                                <p><input type="submit" name="rewind" value="Von vorne" /> (nur nicht-synchronisierte)</p> 
+                                <?php
+                                if ($location === false) {
+                                    echo '<br>Keine weiteren Schlösser zu synchronisieren.<br>Probieren Sie "Von vorne", falls Sie Schlösser<br>übersprungen haben.';
+                                }
+                                ?>
+                            </div>
+                            <img src="../img/lockprogrammer.png">
+                        </div>
+                    </td>
+                    <td>
+                        Vor welchem Schloss stehen wir mit dem Programmiergerät gerade?
+                        <p>
+                            Lock Location:
+                            <select name="lockid">
+                                <?php
+                                $result = $dbh->query("SELECT LockId, Location FROM `lock` ORDER BY Location");
+                                while ($row = $result->fetchObject()) {
+                                    echo '<option value="' . $row->LockId . '"' .
+                                        (($row->LockId == $lockid) ? ' selected' : '') . '>' . 
+                                        $row->Location . '</option>';
+                                }
+                                ?>
+                            </select>                            
+                            <input type="submit" name="apply" value="Schloss auswählen" />
+                            <a href="lock.php?lockid=<?php xecho($lockid); ?>">Link zum Schloss</a>
+                        </p>
                     </td>
                 </tr>
-            </table>
+            </table>  
         </form>
     </body>
 </html>
