169426f4d4d70cea758356d342cdf6b8c1e09346
Jakob Schöttl <jschoett@gmail.com>
2012-12-28 17:37:48 +0100

    Link zum Zugangssystem

--- src/sim/index.php
+++ src/sim/index.php
@@ -11,6 +11,9 @@
             <li><a href="lock.php">Türen/Schlösser ansehen, aufsperren und wieder zumachen</a></li>
             <li><a href="lockprogr.php">Schlösser programmieren (mit Schloss-Programmiergerät)</a></li>
             <li><a href="keyprogr.php">Das Programmier- und Lesegerät für Schlüssel</a></li>
+            <li><a href="buchungssystem.php">Benutzer anlegen (Buchungs-/Zugangssystem)</a></li>
+			<hr>
+            <li><a href="debug">DEBUG (Sessiondaten anzeigen)</a></li>
         </ul>
     </body>
 </html>
