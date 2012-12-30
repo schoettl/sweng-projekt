75e36070c875efdf3e4a4794fa916ea950c37972
Jakob Schöttl <jschoett@gmail.com>
2012-12-30 16:01:29 +0100

    Paar weniger Locks reichen auch zum Testen
    Einfacher und besser zum Programmieren

--- db-test-data-script.sql
+++ db-test-data-script.sql
@@ -16,4 +16,8 @@ insert access values (2, 2, 3, NOW(), '2012-12-23 00:37:06');
 -- active key 25: access fuer lock 2
 insert access values (3, 2, 25, NOW(), null);
 
+-- key 1, 2, 3 und 6, 7, 8 auf whitelist von lock 'R 0. Haupteingang'
+insert whitelist values (31, 1), (31, 2), (31, 3), (31, 6), (31, 7), (31, 8);
+
+
 select * from access;
