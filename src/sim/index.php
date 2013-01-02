ea56ff3263008118fa474a950dce949229e36305
Jakob Schöttl <jschoett@gmail.com>
2013-01-02 15:39:50 +0100

    Debug view should not be linked here

--- src/sim/index.php
+++ src/sim/index.php
@@ -12,8 +12,6 @@
             <li><a href="lockprogr.php">Schlösser programmieren (mit Schloss-Programmiergerät)</a></li>
             <li><a href="keyprogr.php">Das Programmier- und Lesegerät für Schlüssel</a></li>
             <li><a href="buchungssystem.php">Benutzer anlegen (Buchungs-/Zugangssystem)</a></li>
-			<hr>
-            <li><a href="debug">DEBUG (Sessiondaten anzeigen)</a></li>
         </ul>
     </body>
 </html>
